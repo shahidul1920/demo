8e37c0c1f5e91e5c15c00c910b439976125bb762
Shahidul Islam <89517917+shahidul1920@users.noreply.github.com>
2022-11-24 19:19:37 +0600

    xyz

---
+++ app.js
@@ -0,0 +1,18 @@
+let menuTog = document.querySelector('.menutg')
+let menuList = document.querySelector('.mainMenu')
+
+
+menuTog.addEventListener('click', function(){
+    menuTog.classList.toggle('togAct')
+    menuList.classList.toggle('listExt')
+})
+
+
+let loader = document.querySelector(".loader");
+
+window.addEventListener("load", vanish);
+
+function vanish() {
+  loader.classList.add("disppear");
+}
+
